28a082817669b56369e0a1e8a8ba603561f6a6b5
Haardik Aggarwal <haardikaggarwal78@gmail.com>
2025-05-25 22:53:46 +0530

    Add util files

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config(
+    {
+        path: "./.env"
+    }
+)
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`App is listening on PORT:${process.env.PORT || 8000}`)
+    })
+})
+.catch((error)=>{
+    console.log("DB connection failed!! ",error)
+})
