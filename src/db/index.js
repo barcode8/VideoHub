5c843fab4d77ea0ecf21f301faaf73af99e53f37
Haardik Aggarwal <haardikaggarwal78@gmail.com>
2025-06-24 22:11:17 +0530

    Setup for register controller and bug fixes

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,6 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async ()=>{
     try {
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log(`MONGODB Connection Successful DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MONGODB CONNECTION ERROR: ",error);
         process.exit(1)
