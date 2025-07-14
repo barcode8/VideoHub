226db36206a47ff8ae504dba4788ff59a1d5a8f6
Haardik Aggarwal <haardikaggarwal78@gmail.com>
2025-07-14 22:38:58 +0530

    Add user profile and watch history retrieval features

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
 
 const generateAccessAndRefreshTokens= async(userId)=>{
     try {
@@ -229,7 +230,7 @@ const getCurrentUser= asyncHandler(async (req,res)=>{
 
 const changeUserDetails= asyncHandler(async (req,res)=>{
     const {fullName}= req.body
-    const user= User.findByIdAndUpdate(
+    const user= await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
@@ -302,4 +303,116 @@ const changeUserCoverImage= asyncHandler(async (req,res)=>{
     )
 })
 
-export {registerUser,loginUser,logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, changeUserDetails, changeUserAvatar, changeUserCoverImage}
+const getUserProfile= asyncHandler(async (req,res)=>{
+    const username= req.params
+
+    if(!username?.trim()){
+        throw new ApiError(401, "Username not found")
+    }
+
+    const channel= await User.aggregate([{
+        $match:{
+            username: username?.toLowerCase()
+        },
+    },
+    {
+        $lookup:{
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribers"
+        }
+    },
+    {
+        $lookup:{
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "subscriber",
+            as: "subscribedTo"
+        }
+    },
+    {
+        $addFields:{
+            subscribersCount:{
+                $size: "$subscribers"
+            },
+            subscribedToCount:{
+                $size: "$subscribedTo"
+            },
+            isSubscribed:{
+                $cond: {
+                    if: {$in: [req.user?._id, "$subscribers, subscriber"]},
+                    then: true,
+                    else: false
+                }
+            }
+        }
+    },{
+        $project:{
+            fullName: 1,
+            username: 1,
+            subscribersCount: 1,
+            subscribedToCount: 1,
+            avatar: 1,
+            coverImage: 1,
+            isSubscribed: 1,
+        }
+    }])
+
+    if(!channel?.length){
+        throw new ApiError(404,"Channel does not exist")
+    }
+
+    console.log(channel)
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+    )
+})
+
+const getWatchHistory= asyncHandler(async (req,res)=>{
+    const user= User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline:[
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user[0].watchHistory, "User watch history fetched successfully")
+    )
+})
+
+export {registerUser,loginUser,logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, changeUserDetails, changeUserAvatar, changeUserCoverImage, getUserProfile, getWatchHistory}
