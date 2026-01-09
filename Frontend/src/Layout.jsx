52c11bd93a40fa948d391c9a379493f1cd4397e0
Haardik Aggaarwal <haardikaggarwal78@gmail.com>
2026-01-09 14:42:53 +0530

    Add documentation to layout file

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header.jsx'
 import { Outlet } from 'react-router-dom'
 
+//Shared layout for routes that need the header
 function Layout(){
     return(
         <>
