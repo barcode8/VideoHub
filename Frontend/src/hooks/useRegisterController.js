18a763e3ae70f58bb847e5d95613c94de54d3fa5
Haardik Aggaarwal <haardikaggarwal78@gmail.com>
2026-01-12 21:19:08 +0530

    Add documentation to register hook

--- Frontend/src/hooks/useRegisterController.js
+++ Frontend/src/hooks/useRegisterController.js
@@ -13,6 +13,7 @@ export const useRegisterController = () => {
     const [success, setSuccess] = useState(false)
 
     const handleChange = (e) => {
+        //So here what we are doing is that basically whenever onChange is triggered, the object would be set to empty and if we proceed normally with just asignment of one specific attribute that was changed, we would have one valid property and 2 empty ones, hence we use the spread command alongside prev which basically allows us to append the last valid entries of the object and then modify the value of whatever attribute the user targets
         setFormData((prev) => ({
             ...prev,
             [e.target.id]: e.target.value,
@@ -22,6 +23,7 @@ export const useRegisterController = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        //Here we initialise a new FormData variable (IMPORTANT : this approach of FormData only works if one is using multer in that route, otherwise we use normal objects)
         const data = new FormData()
         data.append("username", formData.username);
         data.append("email", formData.email);
@@ -34,7 +36,7 @@ export const useRegisterController = () => {
 
             const res = await axios.post("http://localhost:5000/api/v1/users/register", data, {
                 headers: {
-                    "Content-Type": "multipart/form-data"
+                    "Content-Type": "multipart/form-data" //One part different from other use__ hooks is that here we use multipart/form-data and not application/json since as we discussed before we are using multer here
                 },
                 withCredentials: true
             })
