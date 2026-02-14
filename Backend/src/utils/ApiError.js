5bfa41e1921d3e8094598ef12f0063fa928eca14
Haardik Aggaarwal <haardikaggarwal78@gmail.com>
2026-02-14 21:36:53 +0530

    Add documentation to ApiError utility

--- Backend/src/utils/ApiError.js
+++ Backend/src/utils/ApiError.js
@@ -12,6 +12,7 @@ class ApiError extends Error{
         this.success=false
         this.errors=errors
 
+        //This statement allows us to either use the original stacktrace returned by the JS engine or to create a fresh one that hides all irrelevant internal noise
         if(stack){
             this.stack = stack
         }else{
