4e0b51da27548604200425bbb7e94ede22a799ab
Haardik Aggaarwal <haardikaggarwal78@gmail.com>
2026-02-14 21:38:17 +0530

    Add documentation to ApiResponse utility

--- Backend/src/utils/ApiResponse.js
+++ Backend/src/utils/ApiResponse.js
@@ -1,3 +1,5 @@
+//Allows us to call ApiResponse and sending proper formatted Api response rather than the raw default ones
+
 class ApiResponse {
     constructor(statusCode, data, message = "Success"){
         this.statusCode = statusCode
