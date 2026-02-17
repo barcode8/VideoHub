0bfff90e6b0deb7c74c8972216b6eb618b6d394d
Haardik Aggaarwal <haardikaggarwal78@gmail.com>
2026-02-17 21:04:46 +0530

    Add documentation to user model

--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -47,6 +47,7 @@ const userSchema = new Schema({
     }
 },{timestamps: true})
 
+//Here we use model life cycle middleware (we dont need to call them in routes) so that before a password for a user is saved we hash it using bcrypt
 userSchema.pre("save",async function(next){
     if(this.isModified("password")){
         this.password= await bcrypt.hash(this.password,10)
@@ -54,10 +55,12 @@ userSchema.pre("save",async function(next){
     }
 })
 
+//Checks if entered password is correct or not during login, we have to use this since our passwords are stored in a hash value inside our records
 userSchema.methods.isPasswordCorrect= async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
+//Generating access token which will have the values id, email, username and fullname
 userSchema.methods.generateAccessToken= function(){
     return jwt.sign({
         _id: this._id,
@@ -72,6 +75,7 @@ userSchema.methods.generateAccessToken= function(){
     )
 }
 
+//Generating refresh token which will only store _id
 userSchema.methods.generateRefreshToken= function(){
     return jwt.sign({
         _id: this._id,
