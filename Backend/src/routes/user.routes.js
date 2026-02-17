1a97e1006b4f2ac486df0c2e5b856fe055070a81
Haardik Aggaarwal <haardikaggarwal78@gmail.com>
2026-02-17 19:20:17 +0530

    Add documentation to user routes

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -3,8 +3,10 @@ import { changeCurrentPassword, changeUserAvatar, changeUserCoverImage, changeUs
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
+//Initialising Router
 const router= Router()
 
+//Register route config with multer middleware
 router.route("/register").post(
     upload.fields([
         {
@@ -18,26 +20,36 @@ router.route("/register").post(
     ]),
     registerUser)
 
+//Login route config
 router.route('/login').post(
     loginUser
 )
 
+//Logout route config
 router.route("/logout").post(verifyJwt, logoutUser)
 
+//Refresh Token route config
 router.route("/refresh-token").post(refreshAccessToken)
 
+//Change password route config
 router.route("/change-password").post(verifyJwt, changeCurrentPassword)
 
+//Current user route config
 router.route("/current-user").get(verifyJwt, getCurrentUser)
 
+//Changing user details route config, patch is used when we change a part of the existing data
 router.route("/change-details").patch(verifyJwt, changeUserDetails)
 
-router.route("/change-details").patch(verifyJwt, upload.single("avatar"), changeUserAvatar)
+//Changing avatar route config
+router.route("/change-avatar").patch(verifyJwt, upload.single("avatar"), changeUserAvatar)
 
+//Changing cover image route config
 router.route("/change-coverimage").patch(verifyJwt, upload.single("coverImage"), changeUserCoverImage)
 
+//Returning any user's profile route config
 router.route("/c/:username").get(verifyJwt, getUserProfile)
 
+//Watch history route config
 router.route("/watch-history").get(verifyJwt, getWatchHistory)
 
 export default router
