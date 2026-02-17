ceb9d590d4d37c48508989b5d94a4ba4c7b0b93f
Haardik Aggaarwal <haardikaggarwal78@gmail.com>
2026-02-17 21:25:04 +0530

    Add documentation to jwt auth

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -5,14 +5,17 @@ import jwt from "jsonwebtoken";
 
 export const verifyJwt= asyncHandler(async (req, res, next)=>{
     try {
+        //Checks for access token
         const token= req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
     
         if(!token){
             throw new ApiError(401,"Unauthorized request")
         }
     
+        //Here we decode the access token we checked for
         const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
     
+        //By decoding the token we got access to all its fields, with that we can check for a user by searching for an id and we want the record to not contain the password nor the refreshtoken
         const user= await User.findById(decodedToken?._id).select(
             "-password -refreshToken"
         )
@@ -21,9 +24,10 @@ export const verifyJwt= asyncHandler(async (req, res, next)=>{
             throw new ApiError(401,"Invalid access token")
         }
     
+        //This step is extremely crucial as here we create it so that when we call req.user we got user info which is then used in the get-user api
         req.user=user;
         next()
     } catch (error) {
-        throw new ApiError(401,error?.message || "Invalid access token")
+        throw new ApiError(401, "Invalid access token")
     }
 })
