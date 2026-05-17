1f481d6fb87211c9625f8875c0ec0a35ba1724d2
Haardik Aggaarwal <haardikaggarwal78@gmail.com>
2026-05-17 16:51:11 +0530

    Implement comment functionality with CRUD operations in comment controller and routes

---
+++ Backend/src/controllers/comment.controller.js
@@ -0,0 +1,205 @@
+import mongoose from "mongoose"
+import { Comment } from "../models/comment.models.js"
+import ApiError from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const getVideoComments = asyncHandler(async (req, res) => {
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
+
+    const pageNumber = parseInt(page, 10)
+    const limitNumber = parseInt(limit, 10)
+
+    //Check if we recieved video id or not
+    if(!videoId){
+        throw new ApiError(400, "Video Id not received")
+    }
+
+    //Check if recieved videoId is valid or not
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid Video ID format")
+    }
+
+    const pipeline = [
+        {
+            $match : {
+                //Finding all comments for the specific video
+                video : new mongoose.Types.ObjectId(videoId),
+            }
+        },
+        {
+            $lookup:{
+                from : "users",
+                localField : "owner",
+                foreignField : "_id",
+                as : "ownerDetails",
+                pipeline : [
+                    {
+                        $project:{
+                            username: 1,
+                            fullName: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields:{
+                ownerDetails : { $first: "$ownerDetails" }
+            }
+        },
+        {
+            $sort : {createdAt: -1}
+        }
+    ]
+
+    const aggregate = Comment.aggregate(pipeline)
+
+    const options = {
+        page : pageNumber,
+        limit : limitNumber,
+    }
+
+    const result = await Comment.aggregatePaginate(aggregate,options)
+
+    //Send final response
+    return res
+    .status(200)
+    .json(new ApiResponse(200, result, "Comments fetched successfully"))
+})
+
+const addComment = asyncHandler(async (req, res) => {
+    const {videoId} = req.params
+    const {content} = req.body
+    const userId = req.user._id
+
+    //Check if we recieved video id or not
+    if(!videoId){
+        throw new ApiError(400, "Video Id not received")
+    }
+
+    //Check if recieved videoId is valid or not
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid Video ID format")
+    }
+
+    if(!content || content.trim() === ""){
+        throw new ApiError(400, "Comment body is mandatory")
+    }
+
+    //Check if we recieved user id or not
+    if(!userId){
+        throw new ApiError(400, "User not found")
+    }
+
+    //Check if recieved owner ID is valid or not
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user Id")
+    }
+
+    const comment = await Comment.create({
+        content,
+        video : videoId,
+        owner : userId
+    })
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment added successfully"))
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+    const {content} = req.body
+    const {commentId} = req.params
+    const userId = req.user._id
+
+    //Check if we recieved comment id or not
+    if(!commentId){
+        throw new ApiError(400, "Comment Id not received")
+    }
+
+    //Check if recieved commentId is valid or not
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid Comment ID format")
+    }
+
+    //Checking if content is empty/just spaces
+    if(!content || content.trim() === ""){
+        throw new ApiError(400, "Comment body is mandatory")
+    }
+
+    //Find the comment first using comment ID
+    const comment = await Comment.findById(commentId);
+
+    if(!comment){
+        throw new ApiError(400, "Comment could not be found")
+    }
+
+    //We must check for ownership, whether the comment's owner itself is editing the comment or not
+    if(comment.owner.toString() !== userId.toString() ){
+        throw new ApiError(400, "You do not have permission to edit this comment")
+    }
+
+    //Modify the comment content to the new one
+    comment.content = content
+
+    //ValidateBeforeSave checks whether the new content matches the constraints we defined such as required:true or custom regex commands
+    await comment.save({validateBeforeSave : false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment updated successfully"))
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    const {commentId} = req.params
+    const userId = req.user._id
+
+    //Check if we recieved comment id or not
+    if(!commentId){
+        throw new ApiError(400, "Comment Id not received")
+    }
+
+    //Check if recieved commentId is valid or not
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid Comment ID format")
+    }
+
+    //Check if we recieved owner id or not
+    if(!userId){
+        throw new ApiError(400, "Owner Id not received")
+    }
+
+    //Check if recieved ownerId is valid or not
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid Owner ID format")
+    }
+
+    //Finding the comment
+    const comment = await Comment.findById(commentId)
+
+    if(!comment){
+        throw new ApiError(400, "Comment could not be found")
+    }
+
+    //Check whether the comment owner itself is the one deleting the comment
+    if(comment.owner.toString() !== userId.toString()){
+        throw new ApiError(400, "You do not have permission to delete this comment")
+    }
+
+    //Delete the comment directly from here
+    await comment.deleteOne()
+    
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {deletedCommentId : commentId}, "Comment deleted successfully"))
+})
+
+export {
+    getVideoComments, 
+    addComment, 
+    updateComment,
+    deleteComment
+    }
