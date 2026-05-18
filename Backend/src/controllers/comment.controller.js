4fbfbe88fe09e43658d73685f02254ae99401768
Haardik Aggaarwal <haardikaggarwal78@gmail.com>
2026-05-18 13:15:55 +0530

    Import isValidObjectId from mongoose to validate video IDs in comment controller

--- Backend/src/controllers/comment.controller.js
+++ Backend/src/controllers/comment.controller.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import { isValidObjectId } from "mongoose"
 import { Comment } from "../models/comment.models.js"
 import ApiError from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
